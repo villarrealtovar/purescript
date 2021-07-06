0998875fe9ff55f70136428af00f1c6d83e63dea
Jose Villarreal <>
2021-07-06 11:38:53 -0500

    Either type and Eq, Ord and Show typeclass

--- second-project/src/Ch7a.purs
+++ second-project/src/Ch7a.purs
@@ -2,47 +2,30 @@ module Ch7a where
 
 import Data.Eq (class Eq)
 import Data.Generic.Rep (class Generic)
-import Data.Ord (class Ord, Ordering(..), compare)
+import Data.Ord (class Ord)
 import Data.Show (class Show)
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, show, discard, (==), ($), (<), (>), (<=), (||))
+import Prelude (Unit, discard, show, ($), (<),  (<=), (==),  (>), (>=))
 
 data Maybe a = Nothing | Just a
 
-
-
-
 derive instance eqMaybe :: Eq a => Eq (Maybe a)
--- instance eqMaybe :: Eq a => Eq (Maybe a) where
---   -- eq :: a -> a -> Boolean
---   eq Nothing Nothing = true
---   eq (Just x) (Just y) = x == y
---   eq _ _ = false
-
 derive instance ordMaybe :: Ord a => Ord (Maybe a)
--- -- data Ordering = LT | GT | EQ
--- instance ordMaybe :: Ord a => Ord (Maybe a) where
---   --compare :: a -> a -> Ordering
---   compare Nothing Nothing = EQ
---   compare (Just x) (Just y) = compare x y
---   compare Nothing _ = LT
---   compare _ Nothing = GT
-
--- greaterThanOrEq :: ∀ a. Ord a => a -> a -> Boolean
--- greaterThanOrEq x y = cmp == GT || cmp == EQ where cmp = compare x y
-
--- infixl 4 greaterThanOrEq as >=
-
 derive instance genericMaybe :: Generic (Maybe a) _
+
 instance showMaybe :: Show a => Show (Maybe a) where
   show = genericShow
 
--- instance showMaybe :: Show a =>  Show (Maybe a) where
---   -- show :: a -> String
---   show Nothing = "Nothing"
---   show (Just x) = "(Just " <> show x <> ")"
+data Either a b = Left a | Right b
+
+derive instance eqEither :: (Eq a, Eq b) => Eq (Either a b)
+derive instance ordEither :: (Ord a, Ord b) => Ord (Either a b)
+derive instance genericEither :: Generic (Either a b) _
+
+instance showEither :: (Show a, Show b) => Show (Either a b) where
+  show = genericShow
 
 
 test :: Effect Unit
@@ -56,9 +39,12 @@ test = do
   log $ show $ Just 1 < Just 5 -- true
   log $ show $ Just 5 <= Just 5 -- true
   log $ show $ Just 5 > Just 10 -- false
-  -- log $ show $ Just 10 >= Just 10 -- true
+  log $ show $ Just 10 >= Just 10 -- true
   log $ show $ Just 99 > Nothing -- true
   log $ show $ Just 99 < Nothing -- false
   log "-----------------------"
   log $ show $ Just "abc" -- (Just "abc")
   log $ show $ (Nothing :: Maybe Unit) -- Nothing
+  log "-----------------------"
+  log $ show $ (Left "left" :: Either _ Unit)
+  log $ show $ (Right (Just 42) :: Either Unit _)
