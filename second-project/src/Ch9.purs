582e18c93fca27a71e209f527d42daae89a638a0
Jose Villarreal <>
2021-07-08 11:20:56 -0500

    Monoid for AndBool type

--- second-project/src/Ch9.purs
+++ second-project/src/Ch9.purs
@@ -4,7 +4,7 @@ import Data.Generic.Rep (class Generic)
 import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, class Show, class Eq, ($), discard, show)
+import Prelude (class Eq, class Show, Unit, discard, show, ($), (==), (&&))
 
 
 class Semigroup a where
@@ -13,7 +13,7 @@ class Semigroup a where
 infixr 5 append as <>
 
 class Semigroup a <= Monoid a where
-  mempty :: a -> a
+  mempty :: a
 
 
 data AndBool = AFalse | ATrue
@@ -27,12 +27,33 @@ instance semigroupAndBool :: Semigroup AndBool where
   append ATrue ATrue = ATrue
   append _ _ = AFalse
 
+instance monoidAndBool :: Monoid AndBool where
+  mempty = ATrue
+
+
+verifyAndBoolSemigroup :: Effect Unit
+verifyAndBoolSemigroup = do
+  log "Verifying AndBool Semigroup Laws (1 test)"
+  log $ show $ (AFalse <> ATrue) <> ATrue == AFalse <> (ATrue <> ATrue) -- true
+
+verifyAndBoolMonoid :: Effect Unit
+verifyAndBoolMonoid = do
+  log "Verifying AndBool Monoid Laws (2 test)"
+  log $ show $ mempty <> ATrue == ATrue <> mempty && ATrue <> mempty == ATrue
+  log $ show $ mempty <> AFalse == AFalse <> mempty && AFalse <> mempty == AFalse
+
 test :: Effect Unit
 test = do
+  log $ show $ "--------Semigroup------------"
   log $ show $ ATrue <> ATrue -- ATrue
   log $ show $ ATrue <> AFalse -- AFalse
   log $ show $ AFalse <> AFalse -- AFalse
-
+  log $ show $ "--------Monoid------------"
+  log $ show $ mempty <> ATrue == ATrue -- true
+  log $ show $ mempty <> AFalse == ATrue -- false
+  log $ show $ "--------Verifying Laws------------"
+  verifyAndBoolSemigroup
+  verifyAndBoolMonoid
 
 
 
